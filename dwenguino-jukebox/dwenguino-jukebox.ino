66ead254167e996e9551661580e8cb8b019db663
orestiskarap <orestiskarapiperis@gmail.com>
2019-06-01 20:51:24 +0300

    menu alpha version

--- dwenguino-jukebox/dwenguino-jukebox.ino
+++ dwenguino-jukebox/dwenguino-jukebox.ino
@@ -18,6 +18,8 @@
 // #include "supermario.h"
 // #include "tetris.h"
 
+  int m=0;
+  int n=0;
 
 void setup()
 {
@@ -28,41 +30,86 @@ void setup()
   dwenguinoLCD.setCursor(8,1);
   dwenguinoLCD.print(String("Jukebox"));
 
+   Serial.begin(9600);
+
 }
 
 
 void loop()
 {
+    if (digitalRead(SW_S) == PRESSED) {
+    m++;
+    // Serial.print(m);
+    }
+
+    if (digitalRead(SW_N) == PRESSED) {
+    m--;
+    }
+
     if (digitalRead(SW_C) == PRESSED) {
-      noTone(A5);
+    n=m;}
+
+  if (m==0){
+  dwenguinoLCD.clear();
+  dwenguinoLCD.setCursor(0,0);
+  dwenguinoLCD.print(String("Dwenguino"));
+  dwenguinoLCD.setCursor(8,1);
+  dwenguinoLCD.print(String("Jukebox"));
+  }
+  if (m==1){
+   dwenguinoLCD.clear();
+   dwenguinoLCD.setCursor(0,0);
+   dwenguinoLCD.print(String("Game Of Thrones"));
+  }
+
+    if (n==1){
+      // noTone(A5);
+      // for (int i=0; i<3; i++){
       GameOfThrones();
+      // }
+    }
+
+
+  // if (m==2){
+  //  dwenguinoLCD.clear();
+  //  dwenguinoLCD.clear();
+  //  dwenguinoLCD.setCursor(0,0);
+  //  dwenguinoLCD.print(String("Pirates Of The Carrabean"));
+  // }
+
+    if (digitalRead(SW_C) == PRESSED) {
       dwenguinoLCD.clear();
       dwenguinoLCD.setCursor(0,0);
       dwenguinoLCD.print(String("Game Of Thrones"));
-
-    }
-    if (digitalRead(SW_N) == PRESSED) {
       noTone(A5);
-      carrabean();
-      dwenguinoLCD.clear();
-      dwenguinoLCD.setCursor(0,0);
-      dwenguinoLCD.print(String("Carrabean"));
-
+      for (int i=0; i<3; i++){
+      GameOfThrones();
+      }
     }
-    if (digitalRead(SW_S) == PRESSED) {
-      noTone(BUZZER);
-      tone(BUZZER, 340);
-      dwenguinoLCD.clear();
-      dwenguinoLCD.setCursor(0,0);
-      dwenguinoLCD.print(String("340"));
 
-    }
-    if (digitalRead(SW_W) == PRESSED) {
-      noTone(BUZZER);
-      dwenguinoLCD.clear();
-      dwenguinoLCD.setCursor(0,0);
-      dwenguinoLCD.print(String("Stopped"));
+    // if
+    // if (digitalRead(SW_N) == PRESSED) {
+    //   noTone(A5);
+    //   carrabean();
+    //   dwenguinoLCD.clear();
+    //   dwenguinoLCD.setCursor(0,0);
+    //   dwenguinoLCD.print(String("Carrabean"));
 
-    }
+    // }
+    // if (digitalRead(SW_S) == PRESSED) {
+    //   noTone(BUZZER);
+    //   tone(BUZZER, 340);
+    //   dwenguinoLCD.clear();
+    //   dwenguinoLCD.setCursor(0,0);
+    //   dwenguinoLCD.print(String("340"));
+
+    // }
+    // if (digitalRead(SW_W) == PRESSED) {
+    //   noTone(BUZZER);
+    //   dwenguinoLCD.clear();
+    //   dwenguinoLCD.setCursor(0,0);
+    //   dwenguinoLCD.print(String("Stopped"));
+
+    // }
 
 }
--- dwenguino-jukebox/starwars.h
+++ dwenguino-jukebox/starwars.h
@@ -1,68 +1,31 @@
 
 int counter = 0;
+const int buzzerPin = A5;
 
 // void setup()
 // {
 //   //Setup pin modes
-//   pinMode(buzzerPin, OUTPUT);
+   pinMode(buzzerPin, OUTPUT);
 //   // pinMode(ledPin1, OUTPUT);
 //   // pinMode(ledPin2, OUTPUT);
 // }
-
-void starwars()
-{
-
-  //Play first section
-  firstSection();
-
-  //Play second section
-  secondSection();
-
-  //Variant 1
-  beep(f, 250);
-  beep(gS, 500);
-  beep(f, 350);
-  beep(a, 125);
-  beep(cH, 500);
-  beep(a, 375);
-  beep(cH, 125);
-  beep(eH, 650);
-
-  delay(500);
-
-  //Repeat second section
-  secondSection();
-
-  //Variant 2
-  beep(f, 250);
-  beep(gS, 500);
-  beep(f, 375);
-  beep(cH, 125);
-  beep(a, 500);
-  beep(f, 375);
-  beep(cH, 125);
-  beep(a, 650);
-
-  delay(650);
-}
-
 void beep(int note, int duration)
 {
   //Play tone on buzzerPin
   tone(buzzerPin, note, duration);
 
   //Play different LED depending on value of 'counter'
-  if(counter % 2 == 0)
-  {
-    digitalWrite(ledPin1, HIGH);
-    delay(duration);
-    digitalWrite(ledPin1, LOW);
-  }else
-  {
-    digitalWrite(ledPin2, HIGH);
-    delay(duration);
-    digitalWrite(ledPin2, LOW);
-  }
+  // if(counter % 2 == 0)
+  // {
+  //   digitalWrite(ledPin1, HIGH);
+  //   delay(duration);
+  //   digitalWrite(ledPin1, LOW);
+  // }else
+  // {
+  //   digitalWrite(ledPin2, HIGH);
+  //   delay(duration);
+  //   digitalWrite(ledPin2, LOW);
+  // }
 
   //Stop tone on buzzerPin
   noTone(buzzerPin);
@@ -123,4 +86,41 @@ void secondSection()
   beep(cH, 250);
 
   delay(350);
+}
+
+void starwars()
+{
+
+  //Play first section
+  firstSection();
+
+  //Play second section
+  secondSection();
+
+  //Variant 1
+  beep(f, 250);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(a, 125);
+  beep(cH, 500);
+  beep(a, 375);
+  beep(cH, 125);
+  beep(eH, 650);
+
+  delay(500);
+
+  //Repeat second section
+  secondSection();
+
+  //Variant 2
+  beep(f, 250);
+  beep(gS, 500);
+  beep(f, 375);
+  beep(cH, 125);
+  beep(a, 500);
+  beep(f, 375);
+  beep(cH, 125);
+  beep(a, 650);
+
+  delay(650);
 }
