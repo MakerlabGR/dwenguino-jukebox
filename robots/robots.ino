8a8d865e5b58d2952f9beeba8a5476869fd0b327
orestiskarap <orestiskarapiperis@gmail.com>
2019-06-09 18:11:05 +0300

    Changed robots.ino to support tempo change by a knob.

--- robots/robots.ino
+++ robots/robots.ino
@@ -5,80 +5,96 @@
 #include "pitches1.h"
 
 
-int const speakerPin = A5;
-int tempo = 115.5;
-boolean continuePlaying = true; //Set to true to continuously play (otherwise, false)
-int continueDelay = 0; //Time to wait before continuing playing
-
-void loop(){
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    rest(EIGHTHNOTE);
-
-    spacedNote(NOTE_F3, SIXTEENTHNOTE);
-    spacedNote(NOTE_F3, SIXTEENTHNOTE);
-    rest(EIGHTHNOTE);
-
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_C3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
-    spacedNote(NOTE_D3, SIXTEENTHNOTE);
+bool strobe_effect = false;
+int ledpin1 = 25;
+int ledpin2 = 26;
+int ledpin3 = 27;
+int speakerPin = A5;
+float tempo = 115.5;
 
 
-    rest(EIGHTHNOTE);
+void rest(int restLength) {
+    noTone(speakerPin);
+    float delayTime = (1000.0/tempo) * (60.0/4) * restLength;
+    delay(delayTime);
+}
 
-  noTone(speakerPin);
-  while(continuePlaying == false);
-  delay(continueDelay);
+void spacedNote(int frequencyInHertz, int noteLength, int ledpin) {
+    if (digitalRead(SW_E) == PRESSED) {
+        while(digitalRead(SW_E) == PRESSED) {
+            delay(10);
+        }
+        strobe_effect = !strobe_effect;
+    }
+
+    tempo = analogRead(A0);
+    long outputValue = map(tempo, 0, 1023, 500, 2000);
+    tempo = outputValue/10.0;
+    // Serial.println(strobe_effect);
+    dwenguinoLCD.setCursor(0,1);
+    dwenguinoLCD.print(tempo);
+
+    tone(speakerPin, frequencyInHertz);
+
+    analogWrite(ledpin, 255);
+
+    float delayTime = (1000.0/tempo) * (60.0/4) * noteLength;
+    delay(delayTime - 50);
+
+    analogWrite(ledpin, 0);
+
+    noTone(speakerPin);
+    delay(50);
 }
 
 void setup(){
     initDwenguino();
-    
+
     dwenguinoLCD.setCursor(0,0);
     dwenguinoLCD.print(String("The Robots 1"));
-    dwenguinoLCD.setCursor(0,1);
-    dwenguinoLCD.print(String("Kraftwerk"));
-    
+
     pinMode(speakerPin, OUTPUT);
-    Serial.begin(9600);
+    pinMode(A0, INPUT);
+    pinMode(ledpin1, OUTPUT);
+    pinMode(ledpin2, OUTPUT);
+    pinMode(ledpin3, OUTPUT);
+    // Serial.begin(9600);
 }
 
-void spacedNote(int frequencyInHertz, int noteLength)
-    {
-      tone(speakerPin, frequencyInHertz);
-      float delayTime = (1000.0/tempo) * (60.0/4) * noteLength;
-      delay(delayTime - 50);
-      noTone(speakerPin);
-      delay(50);
-    }
-    // void note(int frequencyInHertz, int noteLength)  //Code to take care of the note
-    // {
-    //   tone(speakerPin, frequencyInHertz);
-    //   float delayTime = (1000.0/tempo) * (60.0/4) * noteLength;
-    //   delay(delayTime);
-    // }
-    void rest(int restLength)
-    {
-      noTone(speakerPin);
-      float delayTime = (1000.0/tempo) * (60.0/4) * restLength;
-      delay(delayTime);
-    }
+void loop(){
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    rest(EIGHTHNOTE);
+
+    spacedNote(NOTE_F3, SIXTEENTHNOTE, ledpin2);
+    spacedNote(NOTE_F3, SIXTEENTHNOTE, ledpin2);
+    rest(EIGHTHNOTE);
+
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_C3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+    spacedNote(NOTE_D3, SIXTEENTHNOTE, ledpin1);
+
+
+    rest(EIGHTHNOTE);
+
+    noTone(speakerPin);
+}
